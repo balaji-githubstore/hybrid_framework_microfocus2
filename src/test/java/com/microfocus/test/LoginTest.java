1415832da8c1f817846ee5929e2effdce2288c0f
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-11-02 17:10:27 +0530

    page object - static

--- src/test/java/com/microfocus/test/LoginTest.java
+++ src/test/java/com/microfocus/test/LoginTest.java
@@ -1,33 +1,33 @@
 package com.microfocus.test;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.microfocus.base.AutomationWrapper;
+import com.microfocus.pages.LoginPage;
 import com.microfocus.utilities.DataUtils;
 
 public class LoginTest extends AutomationWrapper {
 
-	@Test(dataProvider = "commonDataProvider",dataProviderClass = DataUtils.class)
-	public void validLoginTest(String username,String password,String expectedUrl) {
+	@Test(dataProvider = "commonDataProvider", dataProviderClass = DataUtils.class)
+	public void validLoginTest(String username, String password, String expectedUrl) {
 
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		LoginPage.enterUsername(driver, username);
+		LoginPage.enterPassword(driver, password);
+		LoginPage.clickOnLogin(driver);
 
 		// wait for page load
 		String actualUrl = driver.getCurrentUrl();
 		Assert.assertEquals(actualUrl, expectedUrl);
 	}
 
-	@Test(dataProvider = "commonDataProvider",dataProviderClass = DataUtils.class)
+	@Test(dataProvider = "commonDataProvider", dataProviderClass = DataUtils.class)
 	public void invalidLoginTest(String username, String password, String expectedError) {
-		driver.findElement(By.name("username")).sendKeys(username);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		LoginPage.enterUsername(driver, username);
+		LoginPage.enterPassword(driver, password);
+		LoginPage.clickOnLogin(driver);
 
-		String actualError = driver.findElement(By.xpath("//p[contains(@class,'alert-content-text')]")).getText();
+		String actualError = LoginPage.getInvalidErrorMessage(driver);
 		Assert.assertEquals(actualError, expectedError);
 	}
 
