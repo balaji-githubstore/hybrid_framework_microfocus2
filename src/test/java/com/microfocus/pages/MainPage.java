8ca1a3ef8bfc211e0b0853b3078e128f70c18257
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-11-03 09:48:49 +0530

    page object model update

--- src/test/java/com/microfocus/pages/MainPage.java
+++ src/test/java/com/microfocus/pages/MainPage.java
@@ -1,14 +1,42 @@
 package com.microfocus.pages;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage {
-
+	private By adminLocator=By.xpath("//span[text()='Admin']");
+	private By pimLocator=By.xpath("//span[text()='PIM']");
+	
 	private WebDriver driver;
 
 	public MainPage(WebDriver driver)
 	{
 		this.driver=driver;
 	}
+	
+	public void clickOnAdmin()
+	{
+		driver.findElement(adminLocator).click();
+	}
+	
+	public void clickOnPIM()
+	{
+		driver.findElement(pimLocator).click();
+	}
+	
+	public void waitForPresenceOfAdmin()
+	{
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(60));
+		wait.until(ExpectedConditions.presenceOfElementLocated(adminLocator));
+	}
 
+	public String getMainPageUrl()
+	{
+		return driver.getCurrentUrl();
+	}
+	
 }
--- src/test/java/com/microfocus/test/LoginTest.java
+++ src/test/java/com/microfocus/test/LoginTest.java
@@ -1,10 +1,16 @@
 package com.microfocus.test;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.microfocus.base.AutomationWrapper;
 import com.microfocus.pages.LoginPage;
+import com.microfocus.pages.MainPage;
 import com.microfocus.utilities.DataUtils;
 
 public class LoginTest extends AutomationWrapper {
@@ -12,24 +18,26 @@ public class LoginTest extends AutomationWrapper {
 	@Test(dataProvider = "commonDataProvider", dataProviderClass = DataUtils.class)
 	public void validLoginTest(String username, String password, String expectedUrl) {
 
-		LoginPage login=new LoginPage(driver);
+		LoginPage login = new LoginPage(driver);
 		login.enterUsername(username);
 		login.enterPassword(password);
 		login.clickOnLogin();
-	
-		// wait for page load
-		String actualUrl = driver.getCurrentUrl();
+
+		MainPage main = new MainPage(driver);
+		main.waitForPresenceOfAdmin();
+
+		String actualUrl = main.getMainPageUrl();
 		Assert.assertEquals(actualUrl, expectedUrl);
 	}
 
 	@Test(dataProvider = "commonDataProvider", dataProviderClass = DataUtils.class)
 	public void invalidLoginTest(String username, String password, String expectedError) {
-		
-		LoginPage login=new LoginPage(driver);
+
+		LoginPage login = new LoginPage(driver);
 		login.enterUsername(username);
 		login.enterPassword(password);
 		login.clickOnLogin();
-		
+
 		String actualError = login.getInvalidErrorMessage();
 		Assert.assertEquals(actualError, expectedError);
 	}
